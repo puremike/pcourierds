6ef0711b9d393390f54ae97c143654f2eb7de1c1
puremike <michaelmarketing25@gmail.com>
2025-05-27 06:17:09 -0500

    deleted my existing migration at the start of the project

--- cmd/migrate/migrations/000001_create_users_table.up.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS users (
-    id BIGSERIAL PRIMARY KEY,
-    username TEXT NOT NULL UNIQUE,
-    email TEXT NOT NULL UNIQUE,
-    role TEXT NOT NULL,
-    password TEXT NOT NULL,
-    created_at TIMESTAMP DEFAULT NOW(),
-    updated_at TIMESTAMP DEFAULT NOW()
-);
